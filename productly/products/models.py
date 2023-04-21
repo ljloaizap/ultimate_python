124f3fc7bf0dd0d88ff644bc5b5272cb4c7d27b9
Liliana Loaiza <liliana.loaiza@globant.com>
2023-04-21 07:56:10 -0500

    Add models. Make and run migrations.

--- productly/products/models.py
+++ productly/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    score = models.FloatField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
