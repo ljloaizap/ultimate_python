6eb845e9c78b9a9c489f34003823ae3d7d30a8ef
Liliana Loaiza <liliana.loaiza@globant.com>
2023-04-22 08:31:11 -0500

    Returns json in products/ url

--- productly/products/views.py
+++ productly/products/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
+# /products --> this is a URL
+
 
 def index(request):
-    return HttpResponse('Hello World!')
+    # products = Product.objects.filter(score__gte=3)
+    # products = Product.objects.get(id=1)
+    products = Product.objects.all().values()
+    return JsonResponse(list(products), safe=False)
