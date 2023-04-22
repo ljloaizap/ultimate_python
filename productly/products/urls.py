5707dbf3c229a0ca4a955036fb0cdd62729c8fd0
Liliana Loaiza <liliana.loaiza@globant.com>
2023-04-22 15:57:29 -0500

    Use link to go from index to product detail page

--- productly/products/urls.py
+++ productly/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:product_id>', views.detail, name='product_detail')
+    path('<int:product_id>', views.detail, name='detail')
 ]
