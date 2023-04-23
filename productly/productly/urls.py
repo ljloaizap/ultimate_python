58fee4b94346b155164f68760a0211323eb7eaca
Liliana Loaiza <liliana.loaiza@globant.com>
2023-04-22 20:03:13 -0500

    Add init page / landing page

--- productly/productly/urls.py
+++ productly/productly/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.init, name='init'),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls'))
 ]
